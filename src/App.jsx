7be87b2d07960a94c3cabd96199016783c1dbb7d
Pablo Sellarés <psellares@linets.cl>
2023-02-27 12:59:05 -0300

    Change header and main structure

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,10 @@ const App = () => {
             <Luna className="fill-white" />
           </button>
         </div>
-
-        <form className="mt-8 flex h-full w-full items-center gap-4 rounded-md bg-white px-6 py-4">
+      </header>
+      <main className="container mx-auto mt-8 px-4">
+        {/* TodoCreate */}
+        <form className=" flex h-full w-full items-center gap-4 rounded-md bg-white px-6 py-4">
           <button className="box-content inline-block h-2 w-2 rounded-full border-2 p-2"></button>
           <input
             type="text"
@@ -25,9 +27,9 @@ const App = () => {
             className="w-full text-gray-500 outline-none"
           />
         </form>
-      </header>
-      <main className="container mx-auto mt-8 px-4">
-        <div className="my-6 rounded-md bg-white ">
+
+        {/* TodoList (TodoItem) TodoUpdate & TodoDelete */}
+        <div className="mt-8 rounded-t-md bg-white ">
           <article className="border-b-gray400 flex items-center gap-4 border-b px-6 py-4">
             <button className="box-content inline-block h-2 w-2 rounded-full border-2 p-2"></button>
             <p className="grow text-gray-600">
@@ -45,26 +47,32 @@ const App = () => {
               <CrossIcon />
             </button>
           </article>
-
-          <section className="border-b-gray400 flex items-center justify-between gap-4 px-6 py-4 text-[12px] text-gray-500">
-            <span>5 items left</span>
-            <button>Clear Completed</button>
-          </section>
         </div>
+
+        {/* TodoComputed */}
+        <section className="border-b-gray400 flex items-center justify-between gap-4 rounded-b-md bg-white px-6 py-4 text-[12px] text-gray-500">
+          <span>5 items left</span>
+          <button>Clear Completed</button>
+        </section>
+
+        {/* TodoFilter */}
+
+        <section className="container mx-auto mt-8">
+          <div className=" flex items-center justify-center gap-4 rounded-md bg-white p-4 font-semibold">
+            <button className="text-blue-500">All</button>
+            <button className="text-gray-400 hover:text-blue-500">
+              Active
+            </button>
+            <button className="text-gray-400 hover:text-blue-500">
+              Completed
+            </button>
+          </div>
+        </section>
       </main>
-      <section className="container mx-auto px-4">
-        <div className=" flex items-center justify-center gap-4 rounded-md bg-white p-4 font-semibold">
-          <button className="text-blue-500">All</button>
-          <button className="text-gray-400 hover:text-blue-500">Active</button>
-          <button className="text-gray-400 hover:text-blue-500">
-            Completed
-          </button>
-        </div>
-      </section>
 
-      <p className="mt-10 text-center text-gray-400">
+      <footer className="mt-10 text-center text-gray-400">
         Drag and Drop to reorder list
-      </p>
+      </footer>
     </div>
   );
 };
